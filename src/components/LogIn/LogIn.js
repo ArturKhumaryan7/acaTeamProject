47edcea96ff9831906fea0c7364d59686d3a17b5
Narek <narekolav@mail.ru>
2022-01-19 22:07:20 +0400

    navbarAndLogIn

---
+++ src/components/LogIn/LogIn.js
@@ -0,0 +1,21 @@
+import "./LogIn.css"
+import SignIn from "./SignIn/SignIn";
+import SignUp from "./SignUp/SignUp";
+import { Routes, Route } from "react-router-dom"
+import { useEffect, useState } from "react";
+
+    function LogIn({ hideNavBar }){
+
+        const [showSignUp, setShowSignUp] = useState(false)
+        
+        return(
+            <div className='LogInPage' >
+                { !showSignUp && <SignIn hideNavBar={hideNavBar} showSignUp={() => setShowSignUp(true)}/> }
+                {  showSignUp && <SignUp showSignUp={(status) => setShowSignUp(false)}/> }
+                
+                <div className="logInPictureDiv" id="logInPictureDiv" style={{backgroundImage: "url(" +  window.location.origin + "/events.jpg)"}}></div>
+            </div>
+        )
+    }
+
+export default LogIn;
