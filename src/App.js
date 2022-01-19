c41d10319d6b52b930a81b2d34319836b852dd11
Danielyan-Hovo <danielyan.hovo.2000@gmail.com>
2022-01-19 22:15:37 +0400

    orders

--- src/App.js
+++ src/App.js
@@ -1,8 +1,44 @@
+import React from "react";
+import {Route} from "react-router-dom";
+import {Routes} from "react-router";
+import axios from "axios";
+
+import Home from "./Pages/Home";
+import Liked from "./Pages/Liked";
+import Tickets from "./Pages/Tickets";
+import Ticket from './Pages/Ticket';
+
+
+import './Styles/Header.css'
+import './Styles/Ctegories.css'
+
 
 function App() {
-  return (
+    const [orders, setOrders] = React.useState([]);
+    const [events, setEvents] = React.useState([]);
+
+    React.useEffect(()=>{
+        axios.get('https://61e2a20e3050a10017682205.mockapi.io/events').then(({data})=>{
+            setOrders(data);
+        })
+    },[])
+
+    React.useEffect(()=>{
+        axios.get('https://61e2a20e3050a10017682205.mockapi.io/events').then(({data})=>{
+            setEvents(data);
+        })
+    },[])
+
+
+    return (
     <div className="App">
-      
+        <Routes>
+            <Route path="/" element={<Home/>} exact />
+            <Route path="/Liked" element={<Liked items={events}/>} exact />
+            <Route path="/Tickets" element={<Tickets/>}/>
+            <Route path="/T" element={<Ticket items={events} items2={orders} />}/>
+
+        </Routes>
     </div>
   );
 }
---
+++ src/Components/LikedPage.jsx
@@ -0,0 +1,30 @@
+import React, {useState} from 'react';
+import '../Styles/LikedPage.css'
+import like from '../assets/img/svgexport-9.svg'
+import dislike from '../assets/img/svgexport-10.svg'
+
+const LikedPages = ({imageUrl,price,title,page,location,data}) => {
+    const [liked, setLiked] = useState(true);
+    const onSelectLike=((isLiked)=>{
+        setLiked(!isLiked);
+    })
+    return (
+        <div className='div'>
+            <div className='div1'>
+                <a className='a' href={page} >{title}</a>
+                <h3 className='data'>{data}</h3>
+                <h3 className='place'>{location}</h3>
+                <h3 className='price'>{price}</h3>
+            </div>
+            <div className='div2'>
+                <img className='imga' src={imageUrl}/>
+                <button onClick={()=>onSelectLike((liked))} className={'btnl'}>
+                    <img className={'like'} src={liked?like:dislike}/>
+                </button>
+
+            </div>
+        </div>
+    );
+};
+
+export default LikedPages;
